9befda30732e0227759e42ea0687010d17d8c3a7
Evotianus <evotianus0110@gmail.com>
2024-04-06 13:41:26 +0700

    Homepage done (?)

--- js/script.js
+++ js/script.js
@@ -2,26 +2,82 @@ let currentX = 0;
 let currentY = 0;
 let tgX = 0;
 let tgY = 0;
+let isPlaying = 0;
 
-document.addEventListener('DOMContentLoaded', (e) => {
-    const interactiveBubble = document.querySelector('.interactive')
+const playBtn = document.querySelector(".play-button");
+const pauseBtn = document.querySelector(".pause-button");
 
-    function move() {
-        currentX += (tgX - currentX) / 20
-        currentY += (tgY - currentY) / 20
+const songs = [
+	{
+		path: "../assets/albums/Yoru-Magai.mp3",
+		displayName: "Yoru Magai",
+		artist: "YORUSHIKA",
+	},
+];
 
-        interactiveBubble.style.transform = 'translate(' + Math.round(currentX) + 'px, ' + Math.round(currentY) + 'px)'
+const music = new Audio();
 
-        requestAnimationFrame((e) => {
-            move()
-        })
-    }
-    
-    window.addEventListener('mousemove', (e) => {
-        tgX = e.clientX
-        tgY = e.clientY
-        console.log(tgX)
-    })
+music.src = "../assets/albums/Yoru-Magai.mp3";
 
-    move()
-})
+function pauseMusic() {
+	pauseBtn.style.opacity = "0";
+	playBtn.style.opacity = "1";
+	isPlaying = 0;
+
+	music.pause();
+}
+
+function playMusic() {
+	pauseBtn.style.opacity = "1";
+	playBtn.style.opacity = "0";
+	isPlaying = 1;
+
+	music.play();
+}
+
+function togglePlay() {
+	if (isPlaying) {
+		pauseMusic();
+	} else {
+		playMusic();
+	}
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+	const interactiveBubble = document.querySelector(".interactive");
+
+	function move() {
+		currentX += (tgX - currentX) / 20;
+		currentY += (tgY - currentY) / 20;
+
+		interactiveBubble.style.transform =
+			"translate(" +
+			Math.round(currentX) +
+			"px, " +
+			Math.round(currentY) +
+			"px)";
+
+		requestAnimationFrame((e) => {
+			move();
+		});
+	}
+
+	window.addEventListener("mousemove", (e) => {
+		tgX = e.clientX;
+		tgY = e.clientY;
+		console.log(tgX);
+	});
+
+	move();
+
+	music.addEventListener("ended", () => {
+		console.log("woi");
+		pauseMusic();
+	});
+});
+
+let connectBtn = document.querySelector(".connect");
+
+connectBtn.addEventListener("click", (e) => {
+	console.log("jnck");
+});
